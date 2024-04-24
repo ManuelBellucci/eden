4b745ad8cfcfccf5f39ce8284f33874ad3e792d3
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-04-24 17:15:41 +0200

    Update CallToAction button styles

--- src/components/commons/CallToAction.jsx
+++ src/components/commons/CallToAction.jsx
@@ -1,6 +1,6 @@
 const CallToAction = ({text}) => {
     return (
-        <button type='button' className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-3 py-2 text-center'>{text}</button>
+        <button type='button' className='text-primary-950 bg-primary-500 hover:bg-primary-600 active:bg-primary-700 transition-all font-medium rounded-lg text-sm px-3 py-2 text-center'>{text}</button>
     )
 }
 
