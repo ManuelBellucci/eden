e159bf82e799c692e107beafefea3a7528dc6203
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-04-26 12:35:26 +0200

    Update FeaturedCarousel component import to FeaturedCarousels in Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import FeaturedCarousel from "../../components/home/featuredcarousel/FeaturedCarousel"
+import FeaturedCarousels from "../../components/home/featuredcarousels/FeaturedCarousels"
 import Hero from "../../components/home/hero/Hero"
 import Stats from "../../components/home/stats/Stats"
 import Tipologie from "../../components/home/tipologie/Tipologie"
@@ -9,7 +9,7 @@ const Home = () => {
             <Hero />
             <Tipologie />
             <Stats />
-            <FeaturedCarousel />
+            <FeaturedCarousels />
         </>
     )
 
