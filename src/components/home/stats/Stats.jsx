ddf24c54754efad14a7f0aa9cd7608ef129bbc14
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-04-29 12:09:18 +0200

    Update styling and content in Stats and Tipologie components

--- src/components/home/stats/Stats.jsx
+++ src/components/home/stats/Stats.jsx
@@ -5,19 +5,19 @@ const Stats = () => {
             <h1 className="text-center mb-4 text-xl font-extrabold leading-none tracking-tight text-primary-900 md:text-2xl lg:text-3xl">Lorem ipsum <span className="underline underline-offset-4 decoration-8 decoration-primary-400">dolor asit met</span> consectetur adipisicing elit
             </h1>
             <p className="text-center text-md font-normal mx-auto max-w-xl text-primary-500 lg:text-lg mb-14">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div>
                 <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
-                    <div className="mx-auto flex max-w-xs flex-col gap-y-2 bg-primary-100/50 py-16 px-12 justify-center rounded-xl">
+                    <div className=" flex flex-col gap-y-2 bg-primary-100/50 py-16 px-12 justify-center rounded-xl">
                         <img className='h-10 w-10 self-center mb-4' src="./home.png" alt="home icon" />
                         <dd className="text-3xl font-bold tracking-tight text-primary-400 sm:text-5xl">150</dd>
                         <dt className="text-base leading-7 text-primary-600">gli appartamenti affidati a noi dalla costituzione della nostra società</dt>
                     </div>
-                    <div className="mx-auto flex max-w-xs flex-col gap-y-2 bg-primary-100/50 py-16 px-12 justify-center rounded-xl">
+                    <div className=" flex flex-col gap-y-2 bg-primary-100/50 py-16 px-12 justify-center rounded-xl">
                         <img className='h-10 w-10 self-center mb-4' src="./deal.png" alt="deal icon" />
                         <dd className="text-3xl font-bold tracking-tight text-primary-400 sm:text-5xl">138</dd>
                         <dt className="text-base leading-7 text-primary-600">gli appartamenti venduti e rogitati</dt>
                     </div>
-                    <div className="mx-auto flex max-w-xs flex-col gap-y-2 bg-primary-100/50 py-16 px-12 justify-center rounded-xl">
+                    <div className=" flex flex-col gap-y-2 bg-primary-100/50 py-16 px-12 justify-center rounded-xl">
                         <img className='h-10 w-10 self-center mb-4' src="./feedback.png" alt="feedback icon" />
                         <dd className="text-3xl font-bold tracking-tight text-primary-400 sm:text-5xl">140</dd>
                         <dt className="text-base leading-7 text-primary-600">clienti soddisfatti che ci hanno lasciato una recensione positiva</dt>
--- src/components/home/tipologie/TipologieCard.jsx
+++ src/components/home/tipologie/TipologieCard.jsx
@@ -1,9 +1,9 @@
 const TipologieCard = ({title, subtitle}) => {
     return (
-        <div className="relative flex flex-col justify-center overflow-hidden bg-primary-50">
+        <div className="relative flex flex-col justify-center overflow-hidden">
             <div className="absolute inset-0 bg-center"></div>
-            <div className="group relative m-0 flex h-72 w-86 md:w-60 lg:w-52 xl:w-64 xl:h-80 rounded-xl shadow-xl ring-primary-900/5 sm:mx-auto sm:max-w-lg">
-                <div className="z-10 h-full w-full overflow-hidden rounded-xl border border-primary-200 opacity-80 transition duration-300 ease-in-out group-hover:opacity-100">
+            <div className="group relative m-0 flex h-72 w-full xl:h-96 rounded-xl  ring-primary-900/5 sm:mx-auto sm:max-w-lg">
+                <div className="z-10 h-full w-full overflow-hidden rounded-xl opacity-80 transition duration-300 ease-in-out group-hover:opacity-100">
                     <img src="https://images.unsplash.com/photo-1506187334569-7596f62cf93f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3149&q=80" className="animate-fade-in block h-full w-full scale-100 transform object-cover object-center opacity-100 transition duration-300 group-hover:scale-110" alt="" />
                 </div>
                 <div className="absolute bottom-0 z-20 m-0 pb-4 ps-4 transition duration-300 ease-in-out group-hover:-translate-y-1 group-hover:translate-x-3 group-hover:scale-110">
