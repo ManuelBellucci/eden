2186689ff7e5f0a64996d2548801e0ec793c94e3
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-04-29 12:09:27 +0200

    Refactor Home component to include new components and update layout

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,6 @@
-import FeaturedCarousels from "../../components/home/featuredcarousels/FeaturedCarousels"
+import Featured from "../../components/home/featured/Featured"
 import Hero from "../../components/home/hero/Hero"
+import HowItWorks from "../../components/home/howitworks/HowItWorks"
 import Stats from "../../components/home/stats/Stats"
 import Tipologie from "../../components/home/tipologie/Tipologie"
 
@@ -7,9 +8,13 @@ const Home = () => {
     return (
         <>
             <Hero />
-            <Tipologie />
-            <Stats />
-            <FeaturedCarousels />
+            <div className="lg:mx-14">
+                <Tipologie />
+                <Stats />
+                <Featured sells />
+                <Featured rents />
+                <HowItWorks />
+            </div>
         </>
     )
 
