20f63adbeb453ecfface3d3ae25d2fcbacdff4f4
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-04-29 12:08:51 +0200

    Add Featured component to display featured properties on the home page

---
+++ src/components/home/featured/Featured.jsx
@@ -0,0 +1,78 @@
+import Tag from "../../commons/Tag"
+import { SingleCarousel } from "./SingleCarousel"
+
+const Featured = ({ rents, sells }) => {
+    return (
+        <div className="m-14 pt-24">
+            <h1 className="text-center mb-4 text-xl font-extrabold leading-none tracking-tight text-primary-900 md:text-2xl lg:text-3xl">Lorem ipsum 
+                <span className="underline underline-offset-4 decoration-8 decoration-primary-400">dolor asit met</span> consectetur adipisicing elit
+            </h1>
+            <p className="text-center text-md font-normal mx-auto max-w-xl text-primary-500 lg:text-lg mb-14">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+
+            <div className="grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-3">
+                {/* Single card */}
+                <div className="flex flex-col">
+                    <Tag
+                        text="Libero subito"
+                        color="bg-primary-500"
+                    />
+                    <SingleCarousel />
+                    <hgroup className="mt-4">
+                        <h3 className="text-2xl font-bold">Villa</h3>
+                        <p>Via dei Colli, Bologna</p>
+                        <div className="border-t mt-2 pt-2 text-lg flex justify-between">
+                            <div className="flex">
+                                <span className='border-r px-2 flex items-center gap-2'><img src="./double-bed.png" className="h-6 w-6" /> 3 </span>
+                                <span className='border-r px-2 flex items-center gap-2'><img src="./toilet.png" className="h-6 w-6" /> 2 </span>
+                                <span className='border-r px-2 flex items-center gap-2'><img src="./ruler.png" className="h-6 w-6" /> 120 </span>
+                            </div>
+                            <span className="text-2xl font-extrabold text-primary-400">{rents && "800€/mese"}{sells && "139.000€"}</span>
+                        </div>
+                    </hgroup>
+                </div>
+                {/* Single card */}
+                <div className="flex flex-col">
+                    <Tag
+                        text="Nuova costruzione"
+                        color="bg-primary-500"
+                    />
+                    <SingleCarousel />
+                    <hgroup className="mt-4">
+                        <h3 className="text-2xl font-bold">Villa</h3>
+                        <p>Via dei Colli, Bologna</p>
+                        <div className="border-t mt-2 pt-2 text-lg flex justify-between">
+                            <div className="flex">
+                                <span className='border-r px-2 flex items-center gap-2'><img src="./double-bed.png" className="h-6 w-6" /> 3 </span>
+                                <span className='border-r px-2 flex items-center gap-2'><img src="./toilet.png" className="h-6 w-6" /> 2 </span>
+                                <span className='border-r px-2 flex items-center gap-2'><img src="./ruler.png" className="h-6 w-6" /> 120 </span>
+                            </div>
+                            <span className="text-2xl font-extrabold text-primary-400">{rents && "800€/mese"}{sells && "139.000€"}</span>
+                        </div>
+                    </hgroup>
+                </div>
+                {/* Single card */}
+                <div className="flex flex-col">
+                    <Tag
+                        text="Vista panoramica"
+                        color="bg-primary-500"
+                    />
+                    <SingleCarousel />
+                    <hgroup className="mt-4">
+                        <h3 className="text-2xl font-bold">Villa</h3>
+                        <p>Via dei Colli, Bologna</p>
+                        <div className="border-t mt-2 pt-2 text-lg flex justify-between">
+                            <div className="flex">
+                                <span className='border-r px-2 flex items-center gap-2'><img src="./double-bed.png" className="h-6 w-6" /> 3 </span>
+                                <span className='border-r px-2 flex items-center gap-2'><img src="./toilet.png" className="h-6 w-6" /> 2 </span>
+                                <span className='border-r px-2 flex items-center gap-2'><img src="./ruler.png" className="h-6 w-6" /> 120 </span>
+                            </div>
+                            <span className="text-2xl font-extrabold text-primary-400">{rents && "800€/mese"}{sells && "139.000€"}</span>
+                        </div>
+                    </hgroup>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Featured
