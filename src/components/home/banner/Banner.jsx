7548879a897042df9c9f26aa0308317925bc1358
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-04-30 12:04:14 +0200

    Update styling and content in Banner component

--- src/components/home/banner/Banner.jsx
+++ src/components/home/banner/Banner.jsx
@@ -8,8 +8,8 @@ const Banner = () => {
             backgroundPosition: "center 15%",
         }}>
             <div className="p-8 md:p-32 flex flex-col items-center text-center gap-6 justify-center">
-                <h3 className="text-[60px] font-bold max-w-xl">Scopri la casa dove amerai vivere.</h3>
-                <p className="text-xl">Lorem ipsum dolor sit amet.</p>
+                <h3 className="text-xl sm:text-3xl md:text-4xl lg:text-5xl font-bold max-w-xl">Scopri la casa dove amerai vivere.</h3>
+                <p className="text-md md:text-lg lg:text-xl">Lorem ipsum dolor sit amet.</p>
                 <CallToAction text='Vedi le proprietà' />
             </div>
         </div>
