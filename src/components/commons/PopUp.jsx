96488bd183a296da771dbc7d42e3ca7550a03ffa
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-04-30 16:48:25 +0200

    Update PopUp component with new props and styles

--- src/components/commons/PopUp.jsx
+++ src/components/commons/PopUp.jsx
@@ -1,10 +1,28 @@
-const PopUp = ({ text, readMore, color, href }) => {
+const PopUp = ({ text, readMore, color = 'primary-300', href }) => {
+  const textColor = `hover:text-${color}`
+  const ringColor = `hover:ring-${color}`
+  const containerClasses = [
+    'relative',
+    'rounded-full',
+    'px-3',
+    'py-1',
+    'text-sm',
+    'leading-6',
+    'text-primary-300',
+    'ring-1',
+    textColor,
+    ringColor,
+    'ring-primary-300',
+    'transition-all'
+  ].join(' ')
+
   return (
-    <div className={`relative rounded-full px-3 py-1 text-sm leading-6 text-primary-300 ring-1 hover:text-${color} ring-primary-300 hover:ring-${color} transition-all`}>
+    <div className={containerClasses}>
       {text}
       <a href={href} className='font-semibold text-primary-800 hover:text-primary-700 transition-all'>
         <span className='absolute inset-0' aria-hidden='true' />
-        {' '} {readMore} <span aria-hidden='true' />
+        {' '} {readMore}
+        <span aria-hidden='true' />
       </a>
     </div>
   )
