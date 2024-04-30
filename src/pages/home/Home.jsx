69747ff62d18569dcedf08d918aa9dd70b8005ce
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-04-30 12:26:26 +0200

    linted

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,29 +1,28 @@
-import Banner from "../../components/home/banner/Banner"
-import Featured from "../../components/home/featured/Featured"
-import Hero from "../../components/home/hero/Hero"
-import HowItWorks from "../../components/home/howitworks/HowItWorks"
-import Stats from "../../components/home/stats/Stats"
-import Tipologie from "../../components/home/tipologie/Tipologie"
-import WhyWorkWithUs from "../../components/home/whyworkwithus/WhyWorkWithUs"
+import Banner from '../../components/home/banner/Banner'
+import Featured from '../../components/home/featured/Featured'
+import Hero from '../../components/home/hero/Hero'
+import HowItWorks from '../../components/home/howitworks/HowItWorks'
+import Stats from '../../components/home/stats/Stats'
+import Tipologie from '../../components/home/tipologie/Tipologie'
+import WhyWorkWithUs from '../../components/home/whyworkwithus/WhyWorkWithUs'
 
 const Home = () => {
-    return (
-        <>
-            <Hero />
-            <div className="lg:mx-14">
-                <Tipologie />
-                <Stats />
-                <Featured sells />
-                <Featured rents />
-                <HowItWorks />
-            </div>
-            <Banner />
-            <div className="lg:mx-14">
-                <WhyWorkWithUs />
-            </div>
-        </>
-    )
-
+  return (
+    <>
+      <Hero />
+      <div className='lg:mx-14'>
+        <Tipologie />
+        <Stats />
+        <Featured sells />
+        <Featured rents />
+        <HowItWorks />
+      </div>
+      <Banner />
+      <div className='lg:mx-14'>
+        <WhyWorkWithUs />
+      </div>
+    </>
+  )
 }
 
 export default Home
