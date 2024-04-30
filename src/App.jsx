05693d1ca0c17f5d938d1429096402c22dfbeb23
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-04-30 12:00:05 +0200

    Refactor App component to include Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className='relative'>
       <Navbar />
       <Home />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import Newsletter from "./Newsletter";
+
+export default function Footer() {
+  return (
+    <footer>
+      <Newsletter />
+    </footer>
+  )
+}
