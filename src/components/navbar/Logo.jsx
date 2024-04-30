a583dbd492968a0c0fca9da4fdfc1c2fc11623cf
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-04-30 18:04:03 +0200

    Update Logo component with new size options

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -2,9 +2,11 @@ const Logo = ({ src, title }) => {
   return (
     <a href='/' className='flex items-center space-x-3 rtl:space-x-reverse'>
       <img
-        src={src} className='h-8 md:h-10 lg:h-12'
+        src={src} 
+        className='h-8 md:h-10 lg:h-20'
         alt='Eden House Logo'
       />
+      
       <span className='self-center text-sm md:text-xl lg:text-3xl font-semibold whitespace-nowrap'>{title}</span>
     </a>
   )
