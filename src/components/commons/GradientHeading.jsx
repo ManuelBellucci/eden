97ff2aeda25a1b5080d6d6111e27c93a33ba0286
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-04-30 17:11:37 +0200

    Update GradientHeading component with new props and styles

--- src/components/commons/GradientHeading.jsx
+++ src/components/commons/GradientHeading.jsx
@@ -1,15 +1,29 @@
-const GradientHeading = ({ textOne, gradientPhrase, textTwo, paragraph }) => {
-    return (
-        <>
-            <h1 className='mb-4 text-4xl font-extrabold leading-none tracking-tight text-primary-50 md:text-5xl lg:text-6xl'>{textOne}<span className='text-transparent bg-clip-text bg-gradient-to-b to-primary-500 from-primary-200 via-primary-300'>
-                {' '}{gradientPhrase}{' '}
-            </span> {textTwo}
-            </h1>
-            <p className='text-lg font-medium text-primary-100 lg:text-xl'>
-                {paragraph}
-            </p>
-        </>
-    )
+const GradientHeading = ({
+  textOne,
+  gradientPhrase,
+  textTwo,
+  paragraph,
+  gradientFrom = 'primary-200',
+  gradientVia = 'primary-300',
+  gradientTo = 'primary-500',
+  className = '',
+  headingSizeClasses = 'text-4xl md:text-5xl lg:text-6xl', // Allows for overriding size
+  paragraphSizeClasses = 'text-lg lg:text-xl'
+}) => {
+  return (
+    <>
+      <h1 className={`mb-4 font-extrabold leading-none tracking-tight text-primary-50 ${headingSizeClasses} ${className}`}>
+        {textOne}
+        <span className={`text-transparent bg-clip-text bg-gradient-to-b to-${gradientTo} from-${gradientFrom} via-${gradientVia}`}>
+          {' '}{gradientPhrase}{' '}
+        </span>
+        {textTwo}
+      </h1>
+      <p className={`font-medium text-primary-100 ${paragraphSizeClasses}`}>
+        {paragraph}
+      </p>
+    </>
+  )
 }
 
-export default GradientHeading
+export default GradientHeading;
