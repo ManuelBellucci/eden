6c8a7e68eb73b9aa2e60709360e658f1fe79a172
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-03 15:00:40 +0200

    Update Newsletter component styling and content

--- src/components/footer/Newsletter.jsx
+++ src/components/footer/Newsletter.jsx
@@ -2,12 +2,12 @@ import CallToAction from '../commons/CallToAction'
 
 const Newsletter = () => {
   return (
-    <div className='relative isolate overflow-hidden bg-primary-900 py-16 sm:py-24 lg:py-32'>
-      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
-        <div className='mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2'>
+    <div className='p-14'>
+      <div className='mx-auto max-w-7xl'>
+        <div className='grid max-w-2xl grid-cols-1  gap-y-16 lg:max-w-none lg:grid-cols-2'>
           <div className='max-w-xl lg:max-w-lg'>
-            <h2 className='text-3xl font-bold tracking-tight text-white sm:text-4xl'>Subscribe to our newsletter.</h2>
-            <p className='mt-4 text-lg leading-8 text-gray-300'>
+            <h2 className='text-3xl font-bold tracking-tight text-primary-300 sm:text-4xl'>Subscribe to our newsletter.</h2>
+            <p className='mt-4 text-lg leading-8 text-primary-100'>
               Nostrud amet eu ullamco nisi aute in ad minim nostrud adipisicing velit quis. Duis tempor incididunt
               dolore.
             </p>
@@ -31,7 +31,7 @@ const Newsletter = () => {
               />
             </form>
           </div>
-          <dl className='grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2'>
+          <dl className='grid grid-cols-1 gap-x-16 gap-y-10 sm:grid-cols-2 lg:pt-2'>
             <div className='flex flex-col items-start'>
               <div className='rounded-md bg-white/5 p-2 ring-1 ring-white/10'>
                 <div className='h-6 w-6 text-white bg-primary-500' aria-hidden='true' />
@@ -52,15 +52,6 @@ const Newsletter = () => {
             </div>
           </dl>
         </div>
-      </div>
-      <div className='absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6' aria-hidden='true'>
-        <div
-          className='aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-primary-200 to-primary-400 opacity-30'
-          style={{
-            clipPath:
-                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)'
-          }}
-        />
       </div>
     </div>
   )
