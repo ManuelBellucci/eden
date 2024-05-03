c74ee037f2a397c42e8bd464e2c5b780b9bb207c
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-03 15:00:59 +0200

    feat: Add multi-choice dropdowns to Browser component

--- src/components/home/hero/browser/Browser.jsx
+++ src/components/home/hero/browser/Browser.jsx
@@ -1,41 +1,34 @@
+import CallToAction from "../../../commons/CallToAction"
+import MultiChoiceDropdown from "./MultiChoiceDropdown"
+
 const Browser = () => {
+
   return (
-    <form>
+    <div className="w-full">
       <div className='flex gap-16 text-xl font-bold justify-center mb-4 text-white'>
         <div>Vendita</div>
         <div>Affitto</div>
       </div>
-      <div className='h-full w-full flex flex-col md:flex-row gap-4 rounded-lg md:rounded-full bg-white py-4 px-6'>
-        <select className='rounded-full w-full py-4 border border-primary-200 px-4'>
-          <optgroup label='Tipologia' disabled>
-            <option selected hidden>Tipologia</option>
-          </optgroup>
-          <option>Appartamenti</option>
-          <option>Ville e villini</option>
-          <option>Uffici</option>
-          <option>Magazzini, depositi e box</option>
-        </select>
-        <select className='rounded-full w-full py-4 border border-primary-200 px-4'>
-          <optgroup label='Zona' disabled>
-            <option selected hidden>Zona</option>
-          </optgroup>
-          <option>Zona A</option>
-          <option>Zona B</option>
-          <option>Zona C</option>
-          <option>Zona D</option>
-        </select>
-        <select className='rounded-full w-full py-4 border border-primary-200 px-4'>
-          <optgroup label='Filtri' disabled>
-            <option selected hidden>Filtri</option>
-          </optgroup>
+      <div className='h-full w-full flex flex-col md:flex-row gap-4 rounded-lg md:rounded-full bg-white py-4 px-4'>
+        <MultiChoiceDropdown 
+          label='Zona'
+          className='w-full'
+          options={['Saragozza', 'Savena', 'Centro', 'San Ruffillo']} />
+        <MultiChoiceDropdown 
+          label='Tipologia'
+          className='w-full'
+          options={['Appartamento', 'Ville e villette', 'Negozi', 'Box auto']} />
+        <MultiChoiceDropdown 
+          label='Filtri'
+          className='w-full'
+          options={['option1', 'option2', 'option3', 
+            'option4', 'option5', 'option6', 'option7', 'option8', 'option9', 'option10'
+          ]} />
+        
 
-        </select>
-
-        <button className='bg-primary-500 hover:bg-primary-600 active:bg-primary-700 rounded-full py-4 md:py-0 px-8 text-primary-950  font-bold transition-all'>
-          Cercare
-        </button>
+        <CallToAction text='Cercare' asSubmit size="lg" className="w-full rounded-full"/>
       </div>
-    </form>
+    </div>
   )
 }
 
