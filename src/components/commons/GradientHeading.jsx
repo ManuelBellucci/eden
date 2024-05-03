04619f9d066cf2ad2cea12e882a0294bcb05071f
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-03 15:00:12 +0200

    refactor: Refactor GradientHeading component to improve code structure and readability

--- src/components/commons/GradientHeading.jsx
+++ src/components/commons/GradientHeading.jsx
@@ -1,25 +1,51 @@
+// Function to generate the heading class
+const generateHeadingClass = (headingSizeClasses, className) => {
+  return `mb-4 font-extrabold leading-none tracking-tight text-primary-50 ${headingSizeClasses} ${className}`;
+}
+
+// Function to generate the gradient style
+const generateGradientStyle = (gradientTo, gradientFrom, gradientVia) => {
+  return {
+    backgroundClip: 'text',
+    WebkitBackgroundClip: 'text',
+    color: 'transparent',
+    backgroundImage: `linear-gradient(to bottom, ${gradientFrom}, ${gradientVia}, ${gradientTo})`
+  };
+}
+
+// Function to generate the paragraph class
+const generateParagraphClass = (paragraphSizeClasses) => {
+  return `font-medium text-primary-100 ${paragraphSizeClasses}`;
+}
+
+// The refactored GradientHeading component
 const GradientHeading = ({
   textOne,
   gradientPhrase,
   textTwo,
   paragraph,
-  gradientFrom = 'primary-200',
-  gradientVia = 'primary-300',
-  gradientTo = 'primary-500',
+  gradientFrom,
+  gradientVia,
+  gradientTo,
   className = '',
-  headingSizeClasses = 'text-4xl md:text-5xl lg:text-6xl', // Allows for overriding size
+  headingSizeClasses = 'text-4xl md:text-5xl lg:text-6xl',
   paragraphSizeClasses = 'text-lg lg:text-xl'
 }) => {
+  // Generate dynamic classes and style using the helper functions
+  const headingClass = generateHeadingClass(headingSizeClasses, className);
+  const gradientStyle = generateGradientStyle(gradientTo, gradientFrom, gradientVia);
+  const paragraphClass = generateParagraphClass(paragraphSizeClasses);
+
   return (
     <>
-      <h1 className={`mb-4 font-extrabold leading-none tracking-tight text-primary-50 ${headingSizeClasses} ${className}`}>
+      <h1 className={headingClass}>
         {textOne}
-        <span className={`text-transparent bg-clip-text bg-gradient-to-b to-${gradientTo} from-${gradientFrom} via-${gradientVia}`}>
+        <span style={gradientStyle}>
           {' '}{gradientPhrase}{' '}
         </span>
         {textTwo}
       </h1>
-      <p className={`font-medium text-primary-100 ${paragraphSizeClasses}`}>
+      <p className={paragraphClass}>
         {paragraph}
       </p>
     </>
