23fd1339d2c6c9de18da6766073509d3d6f2ba21
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-03 15:00:30 +0200

    feat: Add FooterLinks component to Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,23 @@
+import FooterLinks from './FooterLinks'
 import Newsletter from './Newsletter'
+import SocialMediaIcons from './SocialMediaIcons'
 
 export default function Footer () {
   return (
-    <footer>
+    <footer className='relative isolate overflow-hidden bg-primary-900 lg:p-28'>
       <Newsletter />
+      <FooterLinks />
+      <SocialMediaIcons />
+      
+      <div className='absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6' aria-hidden='true'>
+        <div
+          className='aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-primary-200 to-primary-400 opacity-30'
+          style={{
+            clipPath:
+                            'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)'
+          }}
+        />
+      </div>
     </footer>
   )
 }
