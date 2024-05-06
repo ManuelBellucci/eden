46f0c60f1c7c06c4ed39849a195c4b0adf72f354
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-06 19:51:13 +0200

    feat: Add routing for Immobili page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
-function App () {
+import Immobili from './pages/immobili/Immobili';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
   return (
     <div className='relative'>
       <Navbar />
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route exact index path='/' element={<Home />} />
+          <Route exact path='/immobili' element={<Immobili />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   )
