cd90fa541599b7f95681c0795eb78a86e42d8a4a
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-06 19:53:08 +0200

    feat: Add Immobili page with property listings

---
+++ src/pages/immobili/Immobili.jsx
@@ -0,0 +1,66 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { SingleCarousel } from '../../components/home/featured/SingleCarousel'
+
+const Immobili = () => {
+    const [immobili, setImmobili] = useState([])
+    // const [filters, setFilters] = useState({
+    //     type: '',
+    //     municipality: '',
+    //     floor: '',
+    //     minExpositions: '',
+    //     hasCellar: '',
+    //     minCommercialSqm: '',
+    //     maxPubPrice: '',
+    //     minBedrooms: '',
+    //     minBathrooms: '',
+    //     hasGarage: '',
+    //     hasGarden: '',
+    //     hasTerraceOrBalcony: '',
+    //     heatingType: ''
+    // })
+
+    useEffect(() => {
+        const fetchImmobili = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/listings')
+                setImmobili(response.data)
+            } catch (error) {
+                console.error('Error fetching immobili:', error)
+            }
+        }
+        fetchImmobili()
+    }, [])
+
+    return (
+        <>
+            <div className="h-screen flex justify-center items-center">
+                <h1 className="text-6xl">I nostri immobili</h1>
+                
+            </div>
+            <div className='m-14'>
+                <div className='grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-3'>
+                    {immobili.map(listing => (
+                        <div key={listing._id} className='flex flex-col relative'>
+                            <SingleCarousel images={listing.images} />
+                            <hgroup className='mt-4'>
+                                <h3 className='text-2xl font-bold'>{listing.title}</h3>
+                                <p>{listing.address}, {listing.municipality}</p>
+                                <div className='border-t mt-2 pt-2 text-lg flex justify-between'>
+                                    <div className='flex'>
+                                        <span className='border-r text-sm px-2 flex items-center gap-2'><img src='./double-bed.png' className='h-6 w-6' alt='bed icon' /> {listing.singleBedrooms + listing.doubleBedrooms} </span>
+                                        <span className='border-r text-sm px-2 flex items-center gap-2'><img src='./toilet.png' className='h-6 w-6' alt='toilet icon' /> {listing.bathrooms} </span>
+                                        <span className='border-r text-sm px-2 flex items-center gap-2'><img src='./ruler.png' className='h-6 w-6' alt='size icon' /> {listing.commercialSqm} mq</span>
+                                    </div>
+                                    <span className='text-xl font-extrabold text-primary-400'>{listing.pubPrice.toLocaleString()}€</span>
+                                </div>
+                            </hgroup>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Immobili
