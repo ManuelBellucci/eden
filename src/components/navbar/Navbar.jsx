892156771344e2ecfdbb65d631a119d723bc8e4e
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-07 20:03:19 +0200

    refactor: Update Navbar component to use react-router-dom's useLocation hook for active page highlighting

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,48 +1,60 @@
+import { useLocation } from 'react-router-dom'
 import CallToAction from '../commons/CallToAction'
 import HamburgerMenu from './HamburgerMenu'
 import Logo from './Logo'
-const Navbar = () => {
-  
-  return (
-    <nav className='absolute w-full z-10 border-b border-primary-200/50'>
-      <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-        <Logo src='/logotemp.png' title='Eden House' />
-        <div className='flex md:order-2 gap-4 rtl:space-x-reverse items-center'>
-          <CallToAction
-            anchor
-            href='https://www.youtube.com/'
-            text='Call to Action'
-            size='lg'
-            rounded='xl'
-          />
-          <HamburgerMenu />
-        </div>
-        <div className='items-center justify-between hidden w-full xl:flex xl:w-auto xl:order-1' id='navbar-cta'>
-          <ul className='flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0'>
-            <li>
-              <a href='/' className='block transition-all py-2 px-3 md:p-0 md:text-primary-100' aria-current='page'>Home</a>
-            </li>
-            <li>
-              <a href='/immobili' className='block transition-all py-2 px-3 md:p-0 md:hover:text-primary-100'>Immobili</a>
-            </li>
-            <li>
-              <a href='/about' className='block transition-all py-2 px-3 md:p-0 md:hover:text-primary-100'>Chi siamo?</a>
-            </li>
-            <li>
-              <a href='/blog' className='block transition-all py-2 px-3 md:p-0 md:hover:text-primary-100'>Blog</a>
-            </li>
-            <li>
-              <a href='/guide' className='block transition-all py-2 px-3 md:p-0 md:hover:text-primary-100'>Le guide</a>
-            </li>
-            <li>
-              <a href='/contact' className='block transition-all py-2 px-3 md:p-0 md:hover:text-primary-100'>Contatti</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
 
-  )
+const Navbar = () => {
+    const location = useLocation()
+
+    const navItems = [
+        { path: '/', label: 'Home' },
+        { path: '/immobili', label: 'Immobili' },
+        { path: '/about', label: 'Chi siamo?' },
+        { path: '/blog', label: 'Blog' },
+        { path: '/guide', label: 'Le guide' },
+        { path: '/contact', label: 'Contatti' }
+    ]
+
+    const isImmobiliPage = location.pathname === '/immobili'
+
+
+    return (
+        <nav className='absolute w-full z-10 border-b border-primary-200/50'>
+            <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+                <Logo src='/logotemp.png' title='Eden House' />
+                <div className='flex xl:order-2 gap-4 rtl:space-x-reverse items-center'>
+                    <CallToAction
+                        anchor
+                        href='https://www.youtube.com/'
+                        text='Call to Action'
+                        size='lg'
+                        rounded='xl'
+                    />
+                    <HamburgerMenu />
+                </div>
+                <div className='items-center justify-between hidden w-full xl:flex xl:w-auto xl:order-1' id='navbar-cta'>
+                    <ul className='flex flex-col items-center justify-center xl:flex-row absolute top-0 h-screen left-0 w-screen xl:w-auto xl:relative xl:h-auto font-medium p-4 bg-white text-6xl xl:text-base xl:bg-transparent rounded-lg xl:space-x-8 rtl:space-x-reverse'>
+                        {navItems.map(item => (
+                            <li key={item.path}>
+                                <a
+                                    href={item.path}
+                                    className={`block transition-all py-2 px-3 ${
+                                        location.pathname === item.path
+                                            ? isImmobiliPage
+                                                ? 'text-primary-500'
+                                                : 'xl:text-primary-100 text-primary-500'
+                                            : 'xl:hover:text-primary-100 hover:text-primary-500'
+                                    }`}
+                                >
+                                    {item.label}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
