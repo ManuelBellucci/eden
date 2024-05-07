9d533bc44b9bf4e2ab43b77fdbb925b110702d82
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-07 11:43:30 +0200

    refactor: Update Featured component to use useFetchListings hook for fetching and displaying featured listings

--- src/components/home/featured/Featured.jsx
+++ src/components/home/featured/Featured.jsx
@@ -1,33 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import Tag from '../../commons/Tag';
-import { SingleCarousel } from './SingleCarousel';
-import axios from 'axios';
-
+import useFetchListings from '../../../hooks/useFetchListings';
+import ListingItem from '../../commons/ListingItem';
 const Featured = () => {
-  const [featured, setFeatured] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  const fetchFeatured = async () => {
-    try {
-      const response = await axios.get('http://localhost:5000/listings?limit=5&page=1'); // Adjust this query as per your needs
-      setFeatured(response.data.listings); // Make sure to access `.listings` here
-      setLoading(false);
-    } catch (error) {
-      console.error('Error fetching featured listings:', error);
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchFeatured();
-  }, []);
+  const { listings: featured, loading, error } = useFetchListings(1, 5);
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
-  if (!Array.isArray(featured)) {
-    return <div>Error: Expected an array of listings</div>;
+  if (error || !Array.isArray(featured)) {
+    return <div>Error: Unable to fetch featured listings</div>;
   }
 
 
@@ -36,27 +17,7 @@ const Featured = () => {
       <h2 className='text-center mb-20 text-xl font-extrabold leading-none tracking-tight text-primary-900 md:text-2xl lg:text-3xl'>Le proposte del mese: <span className='underline underline-offset-4 decoration-8 decoration-primary-400'>in vendita</span></h2>
       <div className='grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-3'>
         {featured.map(listing => (
-          <div key={listing._id} className='flex flex-col relative'>
-            <Tag
-              text={listing.tag}
-              color='bg-primary-500/75'
-              position='topLeft'
-              size='lg'
-            />
-            <SingleCarousel images={listing.images} />
-            <hgroup className='mt-4'>
-              <h3 className='text-2xl font-bold'>{listing.title}</h3>
-              <p>{listing.address}, {listing.municipality}</p>
-              <div className='border-t mt-2 pt-2 text-lg flex justify-between'>
-                <div className='flex'>
-                  <span className='border-r text-sm px-2 flex items-center gap-2'><img src='./double-bed.png' className='h-6 w-6' alt='bed icon' /> {listing.doubleBedrooms} </span>
-                  <span className='border-r text-sm px-2 flex items-center gap-2'><img src='./toilet.png' className='h-6 w-6' alt='toilet icon' /> {listing.bathrooms} </span>
-                  <span className='border-r text-sm px-2 flex items-center gap-2'><img src='./ruler.png' className='h-6 w-6' alt='size icon' /> {listing.commercialSqm} mq</span>
-                </div>
-                <span className='text-xl font-extrabold text-primary-400'>{listing.pubPrice.toLocaleString()}€</span>
-              </div>
-            </hgroup>
-          </div>
+          <ListingItem tag key={listing._id} listing={listing} />
         ))}
       </div>
     </div>
