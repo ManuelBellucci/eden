83e4cb1d22b496e29b0f5ee3edf4a4dba97c8def
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-08 15:17:53 +0200

    add clean filters button

--- src/components/immobili/hero/filters/Filters.jsx
+++ src/components/immobili/hero/filters/Filters.jsx
@@ -74,6 +74,25 @@ const Filters = () => {
         }
     };
 
+    // clean filters
+    const cleanFilters = () => {
+        setContract('');
+        setTipology('');
+        setPrice({ from: '', to: '' });
+        setSize({ from: '', to: '' });
+        setRooms({ from: '', to: '' });
+        setBathrooms('');
+        setFloor('');
+        setExtras({
+            terrazzo: false,
+            balcone: false,
+            ascensore: false,
+            arredato: false,
+            cantina: false,
+            piscina: false
+        });
+    };
+
     return (
         <>
             {/* Desktop Filters */}
@@ -126,12 +145,19 @@ const Filters = () => {
                     selectedExtras={extras}
                     setSelectedExtras={setExtras}
                 />
+                <button
+                    className="px-4 py-2 bg-red-500 text-white rounded-lg shadow hover:bg-red-600 active:bg-red-700"
+                    onClick={cleanFilters}
+                >
+                    Pulisci filtri
+                </button>
+
             </div>
 
             {/* Mobile Filters Button */}
             <div className="flex xl:hidden justify-center mb-4">
                 <button
-                    className="px-4 py-2 bg-primary-500 text-white rounded-lg shadow hover:bg-primary-600"
+                    className="px-4 py-2 bg-primary-500 text-white rounded-lg shadow hover:bg-primary-600 active:bg-primary-700"
                     onClick={toggleModal}
                 >
                     Filtra Annunci
@@ -140,71 +166,81 @@ const Filters = () => {
 
             {/* Mobile Filters Modal */}
             {isModalOpen && (
-                <div className="fixed inset-0 p-4 bg-gray-800 bg-opacity-90 z-[100] flex items-center justify-center">
-                    <div className="bg-white h-full flex flex-col gap-6 relative items-center p-6 rounded-lg shadow-lg w-full max-w-lg">
+                <div className="fixed inset-0 p-4 py-14 bg-gray-800 bg-opacity-90 z-[100] flex items-center justify-center">
+                    <div className="bg-white h-full flex flex-col gap-6 relative justify-between items-center p-6 rounded-lg shadow-lg w-full max-w-lg">
                         <div className="flex justify-between items-center mb-4">
                             <h2 className="text-2xl">Filtri</h2>
                             <button
-                                className="text-xl px-4 py-2 absolute top-5 right-5 text-white bg-primary-500 rounded-lg shadow hover:bg-primary-600"
+                                className="text-xl px-4 py-2 absolute top-5 right-5 text-white bg-red-500 rounded-lg shadow hover:bg-red-600 active:bg-red-700"
                                 onClick={toggleModal}
                             >
                                 &times;
                             </button>
                         </div>
-                        <ContractFilterDropdown
-                            isOpen={openModalFilters.contract}
-                            toggle={() => toggleModalFilter('contract')}
-                            selectedContract={contract}
-                            setSelectedContract={setContract}
-                        />
-                        <TipologyFilterDropdown
-                            isOpen={openModalFilters.tipology}
-                            toggle={() => toggleModalFilter('tipology')}
-                            selectedTipology={tipology}
-                            setSelectedTipology={setTipology}
-                        />
-                        <PriceFilterDropdown
-                            isOpen={openModalFilters.price}
-                            toggle={() => toggleModalFilter('price')}
-                            selectedPrice={price}
-                            setSelectedPrice={setPrice}
-                        />
-                        <SizeFilterDropdown
-                            isOpen={openModalFilters.size}
-                            toggle={() => toggleModalFilter('size')}
-                            selectedSize={size}
-                            setSelectedSize={setSize}
-                        />
-                        <RoomsFilterDropdown
-                            isOpen={openModalFilters.rooms}
-                            toggle={() => toggleModalFilter('rooms')}
-                            selectedRooms={rooms}
-                            setSelectedRooms={setRooms}
-                        />
-                        <BathroomsFilterDropdown
-                            isOpen={openModalFilters.bathrooms}
-                            toggle={() => toggleModalFilter('bathrooms')}
-                            selectedBathrooms={bathrooms}
-                            setSelectedBathrooms={setBathrooms}
-                        />
-                        <FloorFilterDropdown
-                            isOpen={openModalFilters.floor}
-                            toggle={() => toggleModalFilter('floor')}
-                            selectedFloor={floor}
-                            setSelectedFloor={setFloor}
-                        />
-                        <ExtrasFilterDropdown
-                            isOpen={openModalFilters.extras}
-                            toggle={() => toggleModalFilter('extras')}
-                            selectedExtras={extras}
-                            setSelectedExtras={setExtras}
-                        />
-                        <button
-                            className="mt-4 px-4 py-2 bg-primary-500 text-white rounded-lg shadow hover:bg-primary-600"
-                            onClick={toggleModal}
-                        >
-                            Applica filtri
-                        </button>
+                        <div className='flex flex-col gap-4'>
+                            <ContractFilterDropdown
+                                isOpen={openModalFilters.contract}
+                                toggle={() => toggleModalFilter('contract')}
+                                selectedContract={contract}
+                                setSelectedContract={setContract}
+                            />
+                            <TipologyFilterDropdown
+                                isOpen={openModalFilters.tipology}
+                                toggle={() => toggleModalFilter('tipology')}
+                                selectedTipology={tipology}
+                                setSelectedTipology={setTipology}
+                            />
+                            <PriceFilterDropdown
+                                isOpen={openModalFilters.price}
+                                toggle={() => toggleModalFilter('price')}
+                                selectedPrice={price}
+                                setSelectedPrice={setPrice}
+                            />
+                            <SizeFilterDropdown
+                                isOpen={openModalFilters.size}
+                                toggle={() => toggleModalFilter('size')}
+                                selectedSize={size}
+                                setSelectedSize={setSize}
+                            />
+                            <RoomsFilterDropdown
+                                isOpen={openModalFilters.rooms}
+                                toggle={() => toggleModalFilter('rooms')}
+                                selectedRooms={rooms}
+                                setSelectedRooms={setRooms}
+                            />
+                            <BathroomsFilterDropdown
+                                isOpen={openModalFilters.bathrooms}
+                                toggle={() => toggleModalFilter('bathrooms')}
+                                selectedBathrooms={bathrooms}
+                                setSelectedBathrooms={setBathrooms}
+                            />
+                            <FloorFilterDropdown
+                                isOpen={openModalFilters.floor}
+                                toggle={() => toggleModalFilter('floor')}
+                                selectedFloor={floor}
+                                setSelectedFloor={setFloor}
+                            />
+                            <ExtrasFilterDropdown
+                                isOpen={openModalFilters.extras}
+                                toggle={() => toggleModalFilter('extras')}
+                                selectedExtras={extras}
+                                setSelectedExtras={setExtras}
+                            />
+                        </div>
+                        <div className='flex gap-4 mt-4'>
+                            <button
+                                className="px-4 py-2 bg-green-500 text-white rounded-lg shadow hover:bg-green-600"
+                                onClick={toggleModal}
+                            >
+                                Applica filtri
+                            </button>
+                            <button
+                                className="px-4 py-2 bg-red-500 text-white rounded-lg shadow hover:bg-red-600 active:bg-red-700"
+                                onClick={cleanFilters}
+                            >
+                                Pulisci filtri
+                            </button>
+                        </div>
                     </div>
                 </div>
             )}
