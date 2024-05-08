c4f6405217164ecfe99e4e42fd379e4e63ec4242
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-08 17:53:57 +0200

    refactor: Update ImmobiliHero component to use full height and add margin-bottom

--- src/components/immobili/hero/ImmobiliHero.jsx
+++ src/components/immobili/hero/ImmobiliHero.jsx
@@ -2,8 +2,8 @@ import Filters from "./filters/Filters";
 
 const ImmobiliHero = () => {
     return (
-        <div className="h-screen flex flex-col gap-8 justify-center items-center">
-            <h1 className="text-6xl text-center">I nostri immobili</h1>
+        <div className="h-full flex flex-col mb-20 gap-8 justify-center items-center">
+            <h1 className="text-6xl mt-40 text-center">I nostri immobili</h1>
             <Filters />
         </div>
     );
