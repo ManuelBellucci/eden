c267941e7de4e557669ef657bc758520599974d7
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-13 12:00:32 +0200

    refactor: Add useFilters and useListings hooks for managing filters and listings

---
+++ src/hooks/useFilters.js
@@ -0,0 +1,101 @@
+import { useContext, useState, useCallback } from 'react';
+import { FiltersContext } from '../contexts/FiltersContext';
+
+const useFilters = () => {
+    const {
+        contract,
+        setContract,
+        tipology,
+        setTipology,
+        price,
+        setPrice,
+        size,
+        setSize,
+        rooms,
+        setRooms,
+        bathrooms,
+        setBathrooms,
+        floor,
+        setFloor,
+        extras,
+        setExtras,
+        cleanFilters
+    } = useContext(FiltersContext);
+
+    const [openFilter, setOpenFilter] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const [openModalFilters, setOpenModalFilters] = useState({
+        contract: false,
+        tipology: false,
+        price: false,
+        size: false,
+        rooms: false,
+        bathrooms: false,
+        floor: false,
+        extras: false
+    });
+
+    const handleFilterChange = useCallback((setter, value) => {
+        setter(value);
+    }, []);
+
+    const toggleFilter = useCallback((filterName) => {
+        setOpenFilter(prevFilter => prevFilter === filterName ? null : filterName);
+    }, []);
+    
+    const toggleModalFilter = useCallback((filterName) => {
+        setOpenModalFilters(prev => {
+            const isCurrentlyOpen = prev[filterName];
+            const newSettings = Object.fromEntries(
+                Object.keys(prev).map(key => [key, false]) // Close all first
+            );
+            return {...newSettings, [filterName]: !isCurrentlyOpen};
+        });
+    }, []);
+
+    const toggleModal = useCallback(() => {
+        setIsModalOpen((prevIsModalOpen) => !prevIsModalOpen);
+        if (!isModalOpen) {
+            setOpenModalFilters({
+                contract: false,
+                tipology: false,
+                price: false,
+                size: false,
+                rooms: false,
+                bathrooms: false,
+                floor: false,
+                extras: false
+            });
+        }
+    }, [isModalOpen]);
+
+    return {
+        contract,
+        setContract,
+        tipology,
+        setTipology,
+        price,
+        setPrice,
+        size,
+        setSize,
+        rooms,
+        setRooms,
+        bathrooms,
+        setBathrooms,
+        floor,
+        setFloor,
+        extras,
+        setExtras,
+        cleanFilters,
+        openFilter,
+        isModalOpen,
+        openModalFilters,
+        handleFilterChange,
+        toggleFilter,
+        toggleModalFilter,
+        toggleModal
+    };
+};
+
+export default useFilters;
---
+++ src/hooks/useListings.js
@@ -0,0 +1,51 @@
+import { useState, useContext } from 'react';
+import useFetchListings from './useFetchListings';
+import { FiltersContext } from '../contexts/FiltersContext'; 
+const useListings = (listingsPerPage = 9) => {
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const {
+        contract,
+        tipology,
+        price,
+        size,
+        rooms,
+        bathrooms,
+        floor,
+        extras
+    } = useContext(FiltersContext);
+
+    const filters = {
+        contract,
+        tipology,
+        priceFrom: price.from,
+        priceTo: price.to,
+        sizeFrom: size.from,
+        sizeTo: size.to,
+        roomsFrom: rooms.from,
+        roomsTo: rooms.to,
+        bathrooms,
+        floor,
+        ...extras
+    };
+
+    const { listings: immobili, totalListings, loading, error } = useFetchListings(currentPage, listingsPerPage, filters);
+
+    const totalPages = Math.ceil(totalListings / listingsPerPage);
+
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
+    return {
+        immobili,
+        totalListings,
+        loading,
+        error,
+        currentPage,
+        totalPages,
+        handlePageChange
+    };
+};
+
+export default useListings;
