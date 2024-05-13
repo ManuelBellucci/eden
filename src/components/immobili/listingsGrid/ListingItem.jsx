5af64b8b45f9c5dd292a5e83692146f1b05f3bea
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-13 12:00:44 +0200

    refactor: Update ListingItem component to include link to individual listing page

--- src/components/immobili/listingsGrid/ListingItem.jsx
+++ src/components/immobili/listingsGrid/ListingItem.jsx
@@ -1,5 +1,6 @@
 import { SingleCarousel } from '../../home/featured/SingleCarousel'
 import Tag from '../../commons/Tag'
+import { Link } from 'react-router-dom'
 const ListingItem = ({ listing, tag }) => {
     return (
         <div className='flex flex-col relative'>
@@ -9,22 +10,27 @@ const ListingItem = ({ listing, tag }) => {
                 position='topLeft'
                 size='lg'
             />}
-            <SingleCarousel images={listing.images} />
-            <hgroup className='mt-4'>
-                <h3 className='text-xl font-bold'>{listing.title}</h3>
-                <div className='flex justify-between items-center'>
-                    <p className='text-xs md:text-sm'>{listing.address}, {listing.municipality}</p>
-                    <div className='italic text-xs'>Rif. {listing._id}</div>
-                </div>
-                <div className='border-t mt-2 pt-2 text-lg flex justify-between'>
-                    <div className='flex'>
-                        <span className='border-r text-sm px-2 flex items-center gap-2'><img src='./double-bed.png' className='h-6 w-6' alt='bed icon' /> {listing.singleBedrooms + listing.doubleBedrooms} </span>
-                        <span className='border-r text-sm px-2 flex items-center gap-2'><img src='./toilet.png' className='h-6 w-6' alt='toilet icon' /> {listing.bathrooms} </span>
-                        <span className='border-r text-sm px-2 flex items-center gap-2'><img src='./ruler.png' className='h-6 w-6' alt='size icon' /> {listing.commercialSqm} mq</span>
+            <SingleCarousel images={listing.images} id={listing._id} />
+        
+            <Link to={`/immobili/${listing._id}`} >
+                <hgroup className='mt-4'>
+                    <h3 className='text-lg 2xl:text-xl font-bold'>{listing.title}</h3>
+                    <div className='flex justify-between items-center'>
+                        <p className='text-xs'>{listing.address}, {listing.municipality}</p>
+                        <div className='italic text-xs'>Rif. {listing._id}</div>
                     </div>
-                    <span className='text-xl font-extrabold text-primary-400'>{listing.pubPrice.toLocaleString()}€</span>
-                </div>
-            </hgroup>
+                    <div className='border-t mt-2 pt-2 text-lg flex justify-between'>
+                        <div className='flex'>
+                            <span className='text-xs text-nowrap px-1 flex flex-col items-center gap-2'><img src='./double-bed.png' className='h-6 object-contain' alt='bed icon' /> {listing.singleBedrooms + listing.doubleBedrooms} </span>
+                            <span className='text-xs text-nowrap px-1 flex flex-col items-center gap-2'><img src='./toilet.png' className='h-6 object-contain' alt='toilet icon' /> {listing.bathrooms} </span>
+                            <span className='text-xs text-nowrap px-1 flex flex-col items-center gap-2'><img src='./ruler.png' className='h-6 object-contain' alt='size icon' /> {listing.commercialSqm} mq</span>
+                            <span className='text-xs text-nowrap px-1 flex flex-col items-center gap-2'><img src='./floor.png' className='h-6 object-contain' alt='floor icon' /> {listing.floor === 0 ? 'T' : listing.floor} di {listing.buildingFloors}</span>
+                            <span className='text-xs text-nowrap px-1 flex flex-col items-center gap-2'><img src='./elevator.png' className='h-6 object-contain' alt='elevator icon' /> {listing.elevator === true ? 'Si' : 'No'}</span>
+                        </div>
+                        <span className='text-lg xl:text-xl self-center font-extrabold text-primary-400'>{listing.pubPrice.toLocaleString()}€</span>
+                    </div>
+                </hgroup>
+            </Link>
         </div>
     )
 }
