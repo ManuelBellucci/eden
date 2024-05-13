7d6220b5661d3c306ed103fdbb3c6aefded60741
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-13 11:59:44 +0200

    refactor: Update PopUp component to use react-router-dom Link instead of anchor tag

--- src/components/commons/PopUp.jsx
+++ src/components/commons/PopUp.jsx
@@ -11,17 +11,28 @@ const PopUp = ({ text, readMore, color = 'primary-300', href }) => {
     'text-sm',
     'leading-6',
     'text-primary-300',
+    'hover:bg-primary-400',
+    'hover:text-white',
     'ring-1',
     textColor,
     ringColor,
     'ring-primary-300',
+    'hover:ring-primary-400',
     'transition-all'
   ].join(' ')
+
+  const linkClasses = [
+    'font-semibold',
+    'text-primary-800',
+    'hover:text-primary-700',
+    'transition-all'
+  ].join(' ')
+
 
   return (
     <div className={containerClasses}>
       {text}
-      <Link to={href} className='font-semibold text-primary-800 hover:text-primary-700 transition-all'>
+      <Link to={href} className={linkClasses}>
         <span className='absolute inset-0' aria-hidden='true' />
         {' '} {readMore}
         <span aria-hidden='true' />
