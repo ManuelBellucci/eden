63cb9197cdf5c97abd76cb64527865c418be1330
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-13 11:23:30 +0200

    refactor: Update Browser component with dynamic municipality options and filter selection functionality NONUSED

--- src/components/home/hero/browser/Browser.jsx
+++ src/components/home/hero/browser/Browser.jsx
@@ -1,32 +1,103 @@
+import { useEffect, useState } from "react";
 import CallToAction from "../../../commons/CallToAction"
 import MultiChoiceDropdown from "./MultiChoiceDropdown"
+import axios from 'axios'
 
 const Browser = () => {
+  const [municipalities, setMunicipalities] = useState([]);
+  const [selectedMunicipalities, setSelectedMunicipalities] = useState([]);
+  const [selectedTipology, setSelectedTipology] = useState([]);
+  const [selectedFilters, setSelectedFilters] = useState([]);
+  const [contractType, setContractType] = useState('vendita');
+
+  const fetchMunicipalities = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/municipalities');
+      setMunicipalities(response.data);   
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMunicipalities();
+  }, []);
+
+  const toggleContractType = (type) => {
+    setContractType(type);
+  };
+
+  const generateQueryParams = () => {
+    const params = new URLSearchParams();
+
+    // Contract type (vendita/affitto)
+    if (contractType) {
+        params.set('c', contractType);
+    }
+
+    // Tipology (only one should be selected)
+    if (selectedTipology.length > 0) {
+        params.set('t', selectedTipology[0].toLowerCase());
+    }
+
+    // Filters (extras)
+    if (selectedFilters.length > 0) {
+        params.set('extras', selectedFilters.join(' ').toLowerCase());
+    }
+
+    return `/immobili?${params.toString()}`;
+};
 
   return (
     <div className="w-full">
-      <div className='flex gap-16 text-xl font-bold justify-center mb-4 text-white'>
-        <div>Vendita</div>
-        <div>Affitto</div>
+      <div className='flex  text-xl font-bold justify-center text-white'>
+        <button
+          onClick={() => toggleContractType('vendita')}
+          className={`px-4 py-2 rounded-t-lg w-full rounded-b-none rounded-tr-none ${contractType === 'vendita'
+              ? 'bg-white text-primary-500 transition-all ease-in'
+              : 'bg-primary-500 text-white transition-all ease-in'
+            }`}
+        >
+          Vendita
+        </button>
+        <button
+          onClick={() => toggleContractType('affitto')}
+          className={`px-4 py-2 rounded-t-lg w-full rounded-b-none rounded-tl-none ${contractType === 'affitto'
+              ? 'bg-white text-primary-500 transition-all ease-in'
+              : 'bg-primary-500 text-white transition-all ease-in'
+            }`}
+        >
+          Affitto
+        </button>
       </div>
-      <div className='h-full w-full flex flex-col md:flex-row gap-4 rounded-lg md:rounded-full bg-white py-4 px-4'>
-        <MultiChoiceDropdown 
-          label='Zona'
+      <div className='h-full w-full flex flex-col md:flex-row gap-4 rounded-lg rounded-t-none md:rounded-t-none bg-white py-4 px-4'>
+        <MultiChoiceDropdown
+          label='Zona (disponibili)'
           className='w-full'
-          options={['Saragozza', 'Savena', 'Centro', 'San Ruffillo']} />
-        <MultiChoiceDropdown 
+          options={municipalities}
+          selectedOptions={selectedMunicipalities}
+          setSelectedOptions={setSelectedMunicipalities}
+        />
+        <MultiChoiceDropdown
           label='Tipologia'
           className='w-full'
-          options={['Appartamento', 'Ville e villette', 'Negozi', 'Box auto']} />
-        <MultiChoiceDropdown 
+          options={['Appartamenti', 'Attici', 'Ville e villette', 'Uffici e negozi', 'Garage e posti auto']}
+          selectedOptions={selectedTipology}
+          setSelectedOptions={setSelectedTipology}
+          isSingleSelection
+        />
+        <MultiChoiceDropdown
           label='Filtri'
           className='w-full'
-          options={['option1', 'option2', 'option3', 
-            'option4', 'option5', 'option6', 'option7', 'option8', 'option9', 'option10'
-          ]} />
-        
+          options={['Terrazzo', 'Balcone', 'Ascensore',
+            'Arredato', 'Cantina', 'Aria condizionata', 'Affittato', 'Ripostiglio'
+          ]}
+          selectedOptions={selectedFilters}
+          setSelectedOptions={setSelectedFilters}
+        />
 
-        <CallToAction text='Cercare' asSubmit rounded="full" size="lg" className="w-full rounded-full"/>
+
+        <CallToAction text='Cercare' anchor rounded="lg" size="lg" className="w-full rounded-full" href={generateQueryParams()} />
       </div>
     </div>
   )
