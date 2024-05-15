1a85503424838c6514dc31daf27c1ca7df060f0b
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-15 18:13:41 +0200

    refactor: Add SingleImmobile route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,20 @@ import { FiltersProvider } from './contexts/FiltersContext'
 import Home from './pages/home/Home'
 import Immobili from './pages/immobili/Immobili'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SingleImmobile from './pages/immobili/SingleImmobile'
+import ScrollToTop from './helpers/scrollToTop'
 
 function App () {
   return (
     <div className='relative'>
       <BrowserRouter>
+        <ScrollToTop />
         <FiltersProvider>
           <Navbar />
           <Routes>
             <Route exact index path='/' element={<Home />} />
             <Route exact path='/immobili' element={<Immobili />} />
+            <Route exact path='/immobili/:id' element={<SingleImmobile />} />
           </Routes>
           <Footer />
         </FiltersProvider>
