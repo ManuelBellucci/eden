04f95edb061a875f9c0f65878abb2066e37b37ad
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-17 15:39:44 +0200

    refactor: Add generateNextNDays function to dateHelpers module

---
+++ src/helpers/dateHelpers.js
@@ -0,0 +1,16 @@
+import dayjs from 'dayjs'
+
+export const generateNextNDays = (n) => {
+  const dates = []
+  let day = dayjs().add(1, 'day') // Start from tomorrow
+  while (dates.length < n) {
+    if (day.day() !== 0) { // Exclude Sundays (day() returns 0 for Sunday)
+      dates.push({
+        label: day.format('ddd DD MMM'),
+        value: day.format('YYYY-MM-DD')
+      })
+    }
+    day = day.add(1, 'day')
+  }
+  return dates
+}
