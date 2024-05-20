839b99fdf81996e620bfd190e3302abfab14f9f7
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-20 17:40:54 +0200

    refactor: Update Alert component styling for better accessibility and consistency

--- src/components/commons/Alerts.jsx
+++ src/components/commons/Alerts.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export const Alert = ({ type, children, extraClassNames }) => {
+const Alert = ({ type, children, extraClassNames }) => {
   const baseClasses = 'p-4 mb-4 text-sm rounded-lg role="alert"'
   const typeClasses = {
-    info: 'text-blue-800 bg-blue-50 dark:bg-gray-800 dark:text-blue-400',
-    danger: 'text-red-800 bg-red-50 dark:bg-gray-800 dark:text-red-400',
-    success: 'text-green-800 bg-green-50 dark:bg-gray-800 dark:text-green-400',
-    warning: 'text-yellow-800 bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300',
-    dark: 'text-gray-800 bg-gray-50 dark:bg-gray-800 dark:text-gray-300'
+    info: 'text-blue-800 bg-blue-50',
+    danger: 'text-red-800 bg-red-50',
+    success: 'text-green-800 bg-green-50',
+    warning: 'text-yellow-800 bg-yellow-50',
+    dark: 'text-gray-800 bg-gray-50'
   }
 
   return (
@@ -17,13 +17,11 @@ export const Alert = ({ type, children, extraClassNames }) => {
     </div>
   )
 }
-
 Alert.propTypes = {
   type: PropTypes.oneOf(['info', 'danger', 'success', 'warning', 'dark']).isRequired,
   children: PropTypes.node.isRequired,
   extraClassNames: PropTypes.string
 }
-
 Alert.defaultProps = {
   extraClassNames: ''
 }
@@ -35,7 +33,6 @@ export const InfoAlert = ({ bold, text, extraClassNames }) => {
     </Alert>
   )
 }
-
 InfoAlert.propTypes = {
   bold: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired
@@ -48,7 +45,6 @@ export const DangerAlert = ({ bold, text, extraClassNames }) => {
     </Alert>
   )
 }
-
 DangerAlert.propTypes = {
   bold: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired
@@ -61,7 +57,6 @@ export const SuccessAlert = ({ bold, text, extraClassNames }) => {
     </Alert>
   )
 }
-
 SuccessAlert.propTypes = {
   bold: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired
@@ -74,7 +69,6 @@ export const WarningAlert = ({ bold, text, extraClassNames }) => {
     </Alert>
   )
 }
-
 WarningAlert.propTypes = {
   bold: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired
@@ -86,4 +80,8 @@ export const DarkAlert = ({ bold, text, extraClassNames }) => {
       <span className='mx-2'> &#9888; </span> <b> {bold} </b> {text}
     </Alert>
   )
+}
+DarkAlert.propTypes = {
+  bold: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
 }
