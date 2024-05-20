8621fab42dd2fea46c2a64223f8b83c804b3d9a8
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-20 09:36:55 +0200

    refactor: Add times array for selecting specific time slots

--- src/helpers/dateHelpers.js
+++ src/helpers/dateHelpers.js
@@ -3,6 +3,14 @@ import 'dayjs/locale/it'
 
 dayjs.locale('it')
 
+export const times = [
+  { label: 'Qualsiasi', value: 'any' },
+  { label: '9-11', value: '9-11' },
+  { label: '11-13', value: '11-13' },
+  { label: '15-17', value: '15-17' },
+  { label: '17-20', value: '17-20' }
+]
+
 export const generateNextNDays = (n) => {
   const dates = []
   let day = dayjs().add(1, 'day') // Start from tomorrow
