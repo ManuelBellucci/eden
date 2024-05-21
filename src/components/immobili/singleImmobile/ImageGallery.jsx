a7fac2a470341d9bebd2d7a62a8bb63c8e4f15b9
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-21 17:28:52 +0200

    refactor: Add container div to ImageGallery component for better styling control

--- src/components/immobili/singleImmobile/ImageGallery.jsx
+++ src/components/immobili/singleImmobile/ImageGallery.jsx
@@ -3,7 +3,9 @@ const ImageGallery = ({ listing }) => {
     <>
       <div className='grid gap-4 col-span-2 bg-gray-100 px-4 py-8 rounded-lg'>
         <div className='relative max-w-2xl mx-auto'>
-          <img className='h-auto max-w-2xl mx-auto w-full rounded-lg' src={listing.images[0]} alt='' />
+          <div className='bg-white p-2 shadow-md rounded-lg'>
+            <img className='h-auto max-w-2xl mx-auto w-full rounded-lg' src={listing.images[0]} alt='' />
+          </div>
           <span className='absolute text-primary-100 font-bold hover:text-black bottom-0 right-0 p-2 m-2 lg:p-4 lg:m-4 text-xs border border-primary-300 hover:bg-primary-300 active:bg-primary-200 cursor-pointer rounded-lg transition-all ease-in'>
             Guarda {listing.images.length} foto
           </span>
@@ -14,7 +16,7 @@ const ImageGallery = ({ listing }) => {
         </div>
         <div className='grid grid-cols-3 gap-4 max-w-2xl mx-auto'>
           {listing.images.slice(1, 4).map((image, index) => (
-            <div key={index}>
+            <div key={index} className='bg-white p-2 shadow-md rounded-lg'>
               <img className='h-auto max-w-full w-full rounded-lg' src={image} alt='' />
             </div>
           ))}
