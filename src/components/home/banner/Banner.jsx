503f56e2faa4d33d23392b46e7d280007c6a17f9
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-21 19:37:02 +0200

    chore: Update banner styles for home page

--- src/components/home/banner/Banner.jsx
+++ src/components/home/banner/Banner.jsx
@@ -6,13 +6,15 @@ const Banner = ({ bgImage, title, description, buttonText, buttonLink }) => {
       className='h-full mt-32 bg-cover bg-center'
       style={{ backgroundImage: `url(${bgImage})` }}
     >
-      <div className='p-8 md:p-32 flex flex-col items-center text-center gap-6 justify-center'>
-        <h3 className='text-xl sm:text-3xl md:text-4xl lg:text-5xl font-bold max-w-xl'>{title}</h3>
-        <p className='text-md md:text-lg lg:text-xl'>{description}</p>
+      <div className='p-32 flex flex-col items-center text-center gap-6 justify-center'>
+        <h3 className='text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold max-w-2xl'>{title}</h3>
+        <p className='text-xl md:text-2xl lg:text-3xl'>{description}</p>
         <CallToAction
           href={buttonLink}
           text={buttonText}
           anchor
+          size='xl'
+          rounded='lg'
         />
       </div>
     </div>
