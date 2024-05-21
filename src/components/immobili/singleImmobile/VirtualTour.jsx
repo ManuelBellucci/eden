59e034abdbf83fa0c1ee20769a0f0763da62eecb
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-21 19:37:38 +0200

    feat: Integrate CallToAction component into VirtualTour for navigation

--- src/components/immobili/singleImmobile/VirtualTour.jsx
+++ src/components/immobili/singleImmobile/VirtualTour.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Pannellum } from 'pannellum-react'
 import 'pannellum-react/es/pannellum/css/pannellum.css'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import CallToAction from '../../commons/CallToAction'
 
 const VirtualTour = ({ isVisible, onClose, images, currentSceneIndex, setCurrentSceneIndex }) => {
   if (!isVisible) return null
@@ -20,7 +21,7 @@ const VirtualTour = ({ isVisible, onClose, images, currentSceneIndex, setCurrent
 
   return (
     <div className='fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-75'>
-      <div className='bg-white rounded-lg overflow-hidden shadow-xl transform transition-all aspect-square w-full max-w-2xl'>
+      <div className='bg-white rounded-lg overflow-hidden h-[50%] shadow-xl transform transition-all aspect-square w-full mx-4 max-w-2xl'>
         <div className='px-4 py-3 bg-gray-100 flex justify-between items-center'>
           <button onClick={onClose} className='text-gray-400 hover:text-gray-600'>
             <svg className='h-6 w-6' fill='none' stroke='currentColor' viewBox='0 0 24 24'>
@@ -28,32 +29,33 @@ const VirtualTour = ({ isVisible, onClose, images, currentSceneIndex, setCurrent
             </svg>
           </button>
         </div>
-        <div className='py-4'>
+        <div className='pb-4'>
           <TransitionGroup>
             <CSSTransition key={currentSceneIndex} timeout={500} classNames='fade'>
               <div>
                 <Pannellum
                   width='100%'
-                  height='500px'
+                  height='300px'
                   image={images[currentSceneIndex]}
                   pitch={10}
                   yaw={180}
                   hfov={110}
-                  minHfov={90} // Minimum zoom level
-                  maxHfov={120} // Maximum zoom level
+                  minHfov={90}
+                  maxHfov={120}
                   autoLoad
                   showZoomCtrl={false}
+
                 />
               </div>
             </CSSTransition>
           </TransitionGroup>
-          <div className='flex justify-center mt-4'>
-            <button onClick={handlePrev} className='bg-blue-500 text-white px-4 py-2 rounded-lg mr-2 hover:bg-blue-700 transition duration-200'>
+          <div className='flex justify-center mt-8 gap-4'>
+            <CallToAction text='<' onClick={handlePrev} size='xl'>
               Previous
-            </button>
-            <button onClick={handleNext} className='bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition duration-200'>
+            </CallToAction>
+            <CallToAction text='>' onClick={handleNext} size='xl'>
               Next
-            </button>
+            </CallToAction>
           </div>
         </div>
       </div>
