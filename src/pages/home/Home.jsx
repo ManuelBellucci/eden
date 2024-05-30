d68aefdf18329be51bf702298d346f82644aad6c
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-05-30 19:12:24 +0200

    refactor: Update Home page with new images and content

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -29,14 +29,14 @@ const Home = () => {
         <Featured />
         <HowItWorks
           processSteps={[
-            { title: 'Facci sapere le tue esigenze', description: 'Contattaci per farci sapere cosa stai cercando e spiegarci le tue esigenze.' },
-            { title: 'Capisci il tuo budget', description: 'Omagiamo per i nostri acquirenti una consulenza di mutuo o di vendita in base alle necessità in modo di darvi a conoscere il budget preciso ed effettivo che avete per l\'acquisto.' },
-            { title: 'Visioniamo l\'immobile', description: 'Dopo aver capito le tue esigenze e il tuo budget, ti mostriamo gli immobili che fanno al caso tuo. Se non trovi quello che cerchi, non preoccuparti, continueremo a cercare per te fino a quando non troveremo la casa dei tuoi sogni.' }
+            { title: 'Facci sapere le tue esigenze', description: 'Contattaci per farci sapere cosa stai cercando e spiegarci le tue esigenze.', src: '/esigenze.svg' },
+            { title: 'Capisci il tuo budget', description: 'Omagiamo per i nostri acquirenti una consulenza di mutuo o di vendita in base alle necessità in modo di darvi a conoscere il budget preciso ed effettivo che avete per l\'acquisto.', src: '/budget.svg' },
+            { title: 'Visioniamo l\'immobile', description: 'Dopo aver capito le tue esigenze e il tuo budget, ti mostriamo gli immobili che fanno al caso tuo. Se non trovi quello che cerchi, non preoccuparti, continueremo a cercare per te fino a quando non troveremo la casa dei tuoi sogni.', src: '/visione.svg' }
           ]}
         />
       </div>
       <Banner
-        bgImage='https://www.travelemiliaromagna.it/wp-content/uploads/2021/06/01-Bologna-Piazza-Maggiore-Ph.-@bolognacasamia.jpg'
+        bgImage='/banner.jpg'
         title='Scopri la casa dove amerai vivere.'
         description='Lorem ipsum dolor sit amet.'
         buttonText='Vedi le proprietà'
@@ -45,8 +45,8 @@ const Home = () => {
       <div className='lg:mx-14'>
         <WhyWorkWithUs
           benefits={[
-            { title: 'Expertise and Professionalism', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod?' },
-            { title: 'Tailored Solutions', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod?' }
+            { title: 'Consulenza personalizzata', content: 'Offriamo consulenze su misura per soddisfare le vostre esigenze specifiche, unendo la saggezza del passato con le tecnologie del presente' },
+            { title: 'Gestione immobiliare', content: 'Gestiamo le vostre proprietà con cura e attenzione, massimizzando il valore del vostro investimento.' }
           ]}
         />
       </div>
