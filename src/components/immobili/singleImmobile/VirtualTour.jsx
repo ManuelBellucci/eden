be0e8fcd7d1d7a639dc62b3738fd78637a501a1a
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-06-03 19:54:18 +0200

    virtual tour restyling

--- src/components/immobili/singleImmobile/VirtualTour.jsx
+++ src/components/immobili/singleImmobile/VirtualTour.jsx
@@ -10,18 +10,18 @@ const VirtualTour = ({ isVisible, onClose, images, currentSceneIndex, setCurrent
   const handleNext = () => {
     setTimeout(() => {
       setCurrentSceneIndex((prevIndex) => (prevIndex + 1) % images.length)
-    }, 500) // Duration of the fade effect
+    }, 500)
   }
 
   const handlePrev = () => {
     setTimeout(() => {
       setCurrentSceneIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length)
-    }, 500) // Duration of the fade effect
+    }, 500)
   }
 
   return (
     <div className='fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-75'>
-      <div className='bg-white rounded-lg overflow-hidden h-full shadow-xl transform transition-all aspect-square w-full'>
+      <div className='bg-white rounded-lg overflow-hidden shadow-xl transform transition-all w-full h-full flex flex-col'>
         <div className='px-4 py-3 bg-gray-100 flex justify-between items-center'>
           <button onClick={onClose} className='text-gray-400 hover:text-gray-600'>
             <svg className='h-6 w-6' fill='none' stroke='currentColor' viewBox='0 0 24 24'>
@@ -29,13 +29,13 @@ const VirtualTour = ({ isVisible, onClose, images, currentSceneIndex, setCurrent
             </svg>
           </button>
         </div>
-        <div className='pb-4 flex flex-col'>
-          <TransitionGroup>
-            <CSSTransition key={currentSceneIndex} timeout={500} classNames='fade'>
-              <div>
+        <div className='flex-1 relative'>
+          <TransitionGroup className='h-full'>
+            <CSSTransition key={currentSceneIndex} timeout={500} classNames='fade' className='h-full'>
+              <div className='h-full'>
                 <Pannellum
                   width='100%'
-                  height='100vh'
+                  height='100%'
                   image={images[currentSceneIndex]}
                   pitch={0}
                   yaw={180}
@@ -44,19 +44,17 @@ const VirtualTour = ({ isVisible, onClose, images, currentSceneIndex, setCurrent
                   maxHfov={110}
                   autoLoad
                   showZoomCtrl={false}
-
                 />
               </div>
             </CSSTransition>
           </TransitionGroup>
 
-          <CallToAction className='absolute top-1/2 left-4' text='<' onClick={handlePrev} size='xl'>
+          <CallToAction className='absolute top-1/2 left-4 transform -translate-y-1/2' text='<' onClick={handlePrev} size='xl'>
             Previous
           </CallToAction>
-          <CallToAction className='absolute top-1/2 right-4' text='>' onClick={handleNext} size='xl'>
+          <CallToAction className='absolute top-1/2 right-4 transform -translate-y-1/2' text='>' onClick={handleNext} size='xl'>
             Next
           </CallToAction>
-
         </div>
       </div>
     </div>
