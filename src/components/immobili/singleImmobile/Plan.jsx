57f7ebeb948ab7c08bfad7219af3ec258fc79d0b
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-06-10 09:46:04 +0200

    chore: Remove unnecessary padding in Plan component

--- src/components/immobili/singleImmobile/Plan.jsx
+++ src/components/immobili/singleImmobile/Plan.jsx
@@ -1,6 +1,6 @@
 const Plan = ({ listing }) => {
   return (
-    <div className='py-6 px-6 md:pr-0 rounded-lg'>
+    <div className='py-6 px-6 rounded-lg'>
       <div className='flex justify-center h-full rounded-lg shadow-md bg-white p-4'>
         <img loading='lazy' className='rounded-lg' src={listing.plan} alt='Plan' />
       </div>
