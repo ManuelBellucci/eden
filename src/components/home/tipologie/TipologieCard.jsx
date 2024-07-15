8f44de52599d6be48654a21e0a45d90762860550
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-07-15 17:27:01 +0200

    refactor: Update TipologieCard component to use primary-950 color for border and rounded corner

--- src/components/home/tipologie/TipologieCard.jsx
+++ src/components/home/tipologie/TipologieCard.jsx
@@ -3,7 +3,7 @@ const TipologieCard = ({ title, subtitle, href, imgSrc }) => {
     <a href={href} className=' flex w-full flex-col justify-center overflow-hidden rounded-lg sm:mx-auto sm:max-w-lg'>
       <div className='group relative m-0 flex h-72 w-full xl:h-96'>
         <img loading='lazy' src={imgSrc} alt={title} className='h-full w-full object-cover object-bottom transition-all duration-300 ease-in-out group-hover:scale-110' />
-        <div className='absolute bottom-0 text-center text-balance border-t-2 h-24 flex flex-col items-center justify-center border-black rounded-t-none rounded-lg left-0 z-20 w-full p-2 bg-primary-500'>
+        <div className='absolute bottom-0 text-center text-balance border-t-2 h-24 flex flex-col items-center justify-center border-primary-950 rounded-t-none rounded-lg left-0 z-20 w-full p-2 bg-primary-500'>
           <h2 className='text-xl lg:text-2xl font-bold text-primary-950'>{title}</h2>
           <p className='text-xl text-primary-950'>{subtitle}</p>
         </div>
