5ac4e52caa6a0083716cd8eb0ff25256b56bfc89
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-07-15 17:26:24 +0200

    refactor: Update PropertyIcons component to use text-nowrap class for better layout

--- src/components/immobili/listingsGrid/PropertyIcons.jsx
+++ src/components/immobili/listingsGrid/PropertyIcons.jsx
@@ -1,6 +1,6 @@
 const PropertyIcon = ({ src, label, value }) => {
   return (
-    <span className='px-1 flex flex-col items-center gap-2 text-xs'>
+    <span className='px-1 flex flex-col items-center gap-2 text-xs text-nowrap'>
       <img loading='lazy' src={src} className='object-contain h-8' alt={`${label} icon`} />
       <span className='text-primary-50 text-base'>
         {value}
@@ -14,8 +14,8 @@ const PropertyIcons = ({ listing }) => {
     <div className='flex'>
       <PropertyIcon src='/double-bed.webp' label='Beds' value={listing.singleBedrooms + listing.doubleBedrooms} />
       <PropertyIcon src='/toilet.webp' label='Bathrooms' value={listing.bathrooms} />
-      <PropertyIcon src='/ruler.webp' label='Size' value={`${listing.commercialSqm} mq`} />
-      <PropertyIcon src='/floor.webp' label='Floor' value={`${listing.floor === 0 ? 'T' : listing.floor} di ${listing.buildingFloors}`} />
+      <PropertyIcon src='/ruler.webp' label='Size' value={`${listing.commercialSqm}mq`} />
+      <PropertyIcon src='/floor.webp' label='Floor' value={`${listing.floor === 0 ? 'T' : listing.floor}`} />
       <PropertyIcon src='/elevator.webp' label='Elevator' value={listing.elevator ? 'Si' : 'No'} />
     </div>
   )
