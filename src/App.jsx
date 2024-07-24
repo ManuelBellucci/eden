dea409f100b6e4d495e0bab0f7a3ca53c80aa5b6
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-07-24 19:23:16 +0200

    feat: Add About page to the application

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SingleImmobile from './pages/immobili/SingleImmobile'
 import ScrollToTop from './helpers/scrollToTop'
 import { useState } from 'react'
+import About from './pages/about/About'
 
 function App () {
   const [isNavbarVisible, setIsNavbarVisible] = useState(true)
@@ -19,6 +20,7 @@ function App () {
           {isNavbarVisible && <Navbar />}
           <Routes>
             <Route exact index path='/' element={<Home />} />
+            <Route exact path='/about' element={<About />} />
             <Route exact path='/immobili' element={<Immobili />} />
             <Route exact path='/immobili/:id' element={<SingleImmobile setIsNavbarVisible={setIsNavbarVisible} />} />
           </Routes>
