75a89092c8fb4e0b2733523ba4ee489fbfdff4ae
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-07-25 13:45:19 +0200

    refactor: Update lazy loading image source in HowItWorks component

--- src/components/home/howitworks/HowItWorks.jsx
+++ src/components/home/howitworks/HowItWorks.jsx
@@ -21,7 +21,7 @@ const HowItWorks = ({ processSteps }) => (
       <div className='md:flex md:items-center'>
         <img
           loading='lazy'
-          src='https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80'
+          src='/howitworks.jpg'
           alt='Image'
           className='w-full h-full rounded-lg object-cover min-h-80'
         />
