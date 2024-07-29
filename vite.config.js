6efa15097f7ba6eca8dcb0ca9bc02742eae41312
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-07-29 16:46:01 +0200

    chore: Update Vite configuration to enable compression for better performance

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import compression from 'vite-plugin-compression'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      algorithm: 'brotliCompress', // You can use 'gzip' or 'deflate' as well
+      ext: '.br', // Use .gz for gzip
+      deleteOriginalAssets: false // Keep the original files
+    })
+  ],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    // Ensure that Vite compresses large text assets
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        manualChunks (id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
+    }
   }
 })
