20cc19215bc904beccfe32a65910a8febb7ed31c
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-07-30 15:38:06 +0200

    chore: Update "Lavora con noi" page with correct email address and formatting

--- src/pages/lavoraConNoi/LavoraConNoi.jsx
+++ src/pages/lavoraConNoi/LavoraConNoi.jsx
@@ -3,7 +3,7 @@ const LavoraConNoi = () => {
     <div className='flex flex-col justify-center items-center p-4 text-center'>
       <h2 className='text-center text-balance my-4 text-3xl font-extrabold leading-none text-primary-500 lg:text-4xl'>Lavora con noi</h2>
       <p className='text-primary-50'>Sei interessato a lavorare con noi? Contattaci subito!</p>
-      <p className='text-primary-50'>Invia una mail a <span className='text primary-500 italic'><a href='mailto:edenhousebologna.com'>edenhousebologna@gmail.com</a></span> con allegato il tuo CV e spiegaci in che mansioni vorresti esserci di aiuto!</p>
+      <p className='text-primary-50'>Invia una mail a <span className='text-primary-500 italic'><a href='mailto:edenhousebologna.com'>edenhousebologna@gmail.com</a></span> con allegato il tuo CV e spiegaci in che mansioni vorresti esserci di aiuto!</p>
     </div>
   )
 }
