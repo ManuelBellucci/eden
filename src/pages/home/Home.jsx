9c9568b8d794e2b696391195e9496c2a1850b96c
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-07-30 17:30:43 +0200

    chore: Update lazy loading for components in Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,54 +1,70 @@
-import Banner from '../../components/home/banner/Banner'
-import Featured from '../../components/home/featured/Featured'
-import Hero from '../../components/home/hero/Hero'
-import HowItWorks from '../../components/home/howitworks/HowItWorks'
-import Stats from '../../components/home/stats/Stats'
-import Tipologie from '../../components/home/tipologie/Tipologie'
-import WhyWorkWithUs from '../../components/home/whyworkwithus/WhyWorkWithUs'
+import React, { Suspense, lazy } from 'react'
+
+const Banner = lazy(() => import('../../components/home/banner/Banner'))
+const Featured = lazy(() => import('../../components/home/featured/Featured'))
+const Hero = lazy(() => import('../../components/home/hero/Hero'))
+const HowItWorks = lazy(() => import('../../components/home/howitworks/HowItWorks'))
+const Stats = lazy(() => import('../../components/home/stats/Stats'))
+const Tipologie = lazy(() => import('../../components/home/tipologie/Tipologie'))
+const WhyWorkWithUs = lazy(() => import('../../components/home/whyworkwithus/WhyWorkWithUs'))
 
 const Home = () => {
   return (
     <>
-      <Hero />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Hero />
+      </Suspense>
       <div className='lg:mx-14 bg-primary-950'>
-        <Tipologie
-          tipologieData={[
-            { title: 'Per single', subtitle: 'Piccoli e facili da gestire', href: '/immobili?t=appartamenti&lmin=1&lmax=2', imgSrc: './single.webp' },
-            { title: 'Per coppie', subtitle: 'L\'inizio della vostra storia', href: '/immobili?t=appartamenti&lmin=2&lmax=3', imgSrc: './coppia.webp' },
-            { title: 'Per famiglie', subtitle: 'Qualcuno ha detto bimbi?', href: '/immobili?t=appartamenti&lmin=4', imgSrc: './famiglia.webp' },
-            { title: 'Per i nonni', subtitle: 'Basta scale!', href: '/immobili?t=appartamenti&extras=elevator', imgSrc: './nonni.webp' }
-          ]}
-        />
-        <Stats
-          statsData={[
-            { icon: '/home.webp', number: 75, description: 'appartamenti affidati a noi dalla costituzione della nostra società (2024)' },
-            { icon: '/deal.svg', number: 68, description: 'appartamenti venduti e rogitati con successo dai nostri agenti immobiliari ' },
-            { icon: '/feedback.webp', number: 222, description: 'clienti soddisfatti che ci hanno lasciato una recensione positiva' }
-          ]}
-        />
-        <Featured />
-        <HowItWorks
-          processSteps={[
-            { title: 'Facci sapere le tue esigenze', description: 'Contattaci per farci sapere cosa stai cercando e spiegarci le tue esigenze.', src: '/esigenze.svg' },
-            { title: 'Capisci il tuo budget', description: 'Omagiamo per i nostri acquirenti una consulenza di mutuo o di vendita in base alle necessità in modo di darvi a conoscere il budget preciso ed effettivo che avete per l\'acquisto.', src: '/budget.svg' },
-            { title: 'Visioniamo l\'immobile', description: 'Dopo aver capito le tue esigenze e il tuo budget, ti mostriamo gli immobili che fanno al caso tuo. Se non trovi quello che cerchi, non preoccuparti, continueremo a cercare per te fino a quando non troveremo la casa dei tuoi sogni.', src: '/visione.svg' }
-          ]}
-        />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Tipologie
+            tipologieData={[
+              { title: 'Per single', subtitle: 'Piccoli e facili da gestire', href: '/immobili?t=appartamenti&lmin=1&lmax=2', imgSrc: './single.webp' },
+              { title: 'Per coppie', subtitle: 'L\'inizio della vostra storia', href: '/immobili?t=appartamenti&lmin=2&lmax=3', imgSrc: './coppia.webp' },
+              { title: 'Per famiglie', subtitle: 'Qualcuno ha detto bimbi?', href: '/immobili?t=appartamenti&lmin=4', imgSrc: './famiglia.webp' },
+              { title: 'Per i nonni', subtitle: 'Basta scale!', href: '/immobili?t=appartamenti&extras=elevator', imgSrc: './nonni.webp' }
+            ]}
+          />
+        </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Stats
+            statsData={[
+              { icon: '/home.webp', number: 75, description: 'appartamenti affidati a noi dalla costituzione della nostra società (sett. 2023)' },
+              { icon: '/deal.svg', number: 68, description: 'appartamenti venduti e rogitati con successo dai nostri agenti immobiliari ' },
+              { icon: '/feedback.webp', number: 222, description: 'clienti soddisfatti che ci hanno lasciato una recensione positiva' }
+            ]}
+          />
+        </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Featured />
+        </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+          <HowItWorks
+            processSteps={[
+              { title: 'Facci sapere le tue esigenze', description: 'Contattaci per farci sapere cosa stai cercando e spiegarci le tue esigenze.', src: '/esigenze.svg' },
+              { title: 'Capisci il tuo budget', description: 'Omagiamo per i nostri acquirenti una consulenza di mutuo o di vendita in base alle necessità in modo di darvi a conoscere il budget preciso ed effettivo che avete per l\'acquisto.', src: '/budget.svg' },
+              { title: 'Visioniamo l\'immobile', description: 'Dopo aver capito le tue esigenze e il tuo budget, ti mostriamo gli immobili che fanno al caso tuo. Se non trovi quello che cerchi, non preoccuparti, continueremo a cercare per te fino a quando non troveremo la casa dei tuoi sogni.', src: '/visione.svg' }
+            ]}
+          />
+        </Suspense>
       </div>
-      <Banner
-        bgImage='/banner.webp'
-        title='Scopri la casa dove amerai vivere.'
-        description='Non aspettare, il tuo futuro inizia oggi.'
-        buttonText='Vedi le proprietà'
-        buttonLink='/immobili'
-      />
-      <div className='lg:mx-14'>
-        <WhyWorkWithUs
-          benefits={[
-            { title: 'Consulenza personalizzata', content: 'Offriamo consulenze su misura per soddisfare le vostre esigenze specifiche, unendo la saggezza del passato con le tecnologie del presente' },
-            { title: 'Gestione immobiliare', content: 'Gestiamo le vostre proprietà con cura e attenzione, massimizzando il valore del vostro investimento.' }
-          ]}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Banner
+          bgImage='/banner.webp'
+          title='Scopri la casa dove amerai vivere.'
+          description='Non aspettare, il tuo futuro inizia oggi.'
+          buttonText='Vedi le proprietà'
+          buttonLink='/immobili'
         />
+      </Suspense>
+      <div className='lg:mx-14'>
+        <Suspense fallback={<div>Loading...</div>}>
+          <WhyWorkWithUs
+            benefits={[
+              { title: 'Consulenza personalizzata', content: 'Offriamo consulenze su misura per soddisfare le vostre esigenze specifiche, unendo la saggezza del passato con le tecnologie del presente' },
+              { title: 'Gestione immobiliare', content: 'Gestiamo le vostre proprietà con cura e attenzione, massimizzando il valore del vostro investimento.' }
+            ]}
+          />
+        </Suspense>
       </div>
     </>
   )
