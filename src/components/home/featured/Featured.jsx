c55f536103d10f4147a2ae2453d4c55c3cffdeeb
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-07-30 10:32:50 +0200

    chore: Update Featured component markup for better readability and consistency

--- src/components/home/featured/Featured.jsx
+++ src/components/home/featured/Featured.jsx
@@ -4,7 +4,7 @@ import CardSkeleton from '../../skeletons/CardSkeleton'
 
 const Loading = () => (
   <div className='m-14 mt-24'>
-    <h2 className='text-center mb-20 text-xl font-extrabold leading-none text-primary-50 md:text-2xl lg:text-4xl'>
+    <h2 className='text-center text-balance mb-4 text-3xl font-extrabold leading-none text-primary-50 lg:text-4xl'>
       Le proposte del mese: <span className='text-primary-500'>in vendita</span>
     </h2>
     <div className='grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-3'>
