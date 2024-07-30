67ad2987e193828eb9734658fba8b097c0d52d3a
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-07-30 16:35:08 +0200

    .

--- src/pages/lavoraConNoi/LavoraConNoi.jsx
+++ src/pages/lavoraConNoi/LavoraConNoi.jsx
@@ -2,8 +2,8 @@ const LavoraConNoi = () => {
   return (
     <div className='flex flex-col justify-center items-center p-4 text-center'>
       <h2 className='text-center text-balance my-4 text-3xl font-extrabold leading-none text-primary-500 lg:text-4xl'>Lavora con noi</h2>
-      <p className='text-primary-50'>Sei interessato a lavorare con noi? Contattaci subito!</p>
-      <p className='text-primary-50'>Invia una mail a
+      <p className='text-primary-50 text-2xl'>Sei interessato a lavorare con noi? Contattaci subito!</p>
+      <p className='text-primary-50 text-2xl'>Invia una mail a
         <span className='text-primary-500 italic'>
           <a href='mailto:edenhousebologna.com'> edenhousebologna@gmail.com </a>
         </span>
