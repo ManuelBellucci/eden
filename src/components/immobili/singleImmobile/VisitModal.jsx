11cab96eaddf52c343da5d9b3fb5a2577e763f36
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-09-09 10:25:14 +0200

    Refactor VisitModal component and update API_URL default value

--- src/components/immobili/singleImmobile/VisitModal.jsx
+++ src/components/immobili/singleImmobile/VisitModal.jsx
@@ -5,7 +5,7 @@ import validateEmail from '../../../helpers/validateEmail'
 import { sendEmail } from '../../../helpers/sendEmail'
 import { useParams } from 'react-router-dom'
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
 
 const VisitModal = ({
   isVisible,
