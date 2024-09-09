3ffdcc9bdcb063b269bf50795030cc4e41e8f4de
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-09-09 10:25:03 +0200

    Refactor footer component and add company information
    - Refactor the footer component to include a background image with a specific size.
    - Add company information to the footer, including P.IVA, Cod. Fisc, and the company's address and email.

--- src/components/footer/Newsletter.jsx
+++ src/components/footer/Newsletter.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import CallToAction from '../commons/CallToAction'
 import validateEmail from '../../helpers/validateEmail'
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
 
 const InfoBlock = ({ iconSrc, title, description }) => (
   <div className='flex flex-col items-start'>
--- src/components/footer/SocialMediaIcons.jsx
+++ src/components/footer/SocialMediaIcons.jsx
@@ -51,6 +51,11 @@ const socialMedia = [
 const SocialMediaIcons = () => {
   return (
     <>
+      <div className='text-white text-center mb-4 px-10 text-pretty'>
+        <p>P.IVA 123456789 Cod. Fisc 123456789 | Sede legale: Via Saragozza n.112/O - 40135 Bologna (BO)
+        </p>
+        <p>edenhousebologna@gmail.com</p>
+      </div>
       <div className='flex justify-between items-center px-8 lg:px-14 py-6 lg:py-8'>
         <span className='text-sm text-primary-50'>© 2024 <a href='https://edenhouse.me'>Eden srl</a>. All Rights Reserved.</span>
         <div className='flex justify-center items-center space-x-5 rtl:space-x-reverse'>
@@ -59,11 +64,7 @@ const SocialMediaIcons = () => {
           ))}
         </div>
       </div>
-      <div className='text-white text-center mb-4'>
-        <p>P.IVA 123456789 Cod. Fisc 123456789 | Sede legale: Via Saragozza n.112/O - 40135 Bologna (BO)
-        </p>
-        <p>edenhousebologna@gmail.com</p>
-      </div>
+
     </>
   )
 }
