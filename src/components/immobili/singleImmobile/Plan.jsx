4d59bb9d0518ccb2b1f950f18f883606e38daa07
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-09-27 11:55:10 +0200

    Refactor Plan component to include fullscreen mode for the plan image

--- src/components/immobili/singleImmobile/Plan.jsx
+++ src/components/immobili/singleImmobile/Plan.jsx
@@ -1,13 +1,22 @@
+import { useState } from 'react'
 import { PlanCarousel } from './PlanCarousel'
 
 // Plan - Componente che renderizza la planimetria dell'immobile
 const Plan = ({ listing }) => {
+  // Stato per gestire se l'immagine è in modalità fullscreen
+  const [isFullscreen, setIsFullscreen] = useState(false)
+
   // Verifica se l'annuncio ha più di una planimetria
   const hasMultiplePlans = listing.plan && listing.plan.length > 1
 
+  // Funzione per gestire il clic sull'immagine
+  const handleImageClick = () => {
+    setIsFullscreen(!isFullscreen) // Cambia lo stato della modalità fullscreen
+  }
+
   return (
     <div className='py-6 px-6 md:pr-0 rounded-lg'>
-      <div className='flex justify-center h-full rounded-lg shadow-md bg-primary-50 p-4'>
+      <div className={`flex justify-center h-full rounded-lg shadow-md bg-primary-50 p-4 ${isFullscreen ? 'fullscreen' : ''}`}>
         {/* Carosello delle planimetrie se ce ne sono più di una, altrimenti mostra la singola planimetria */}
         {hasMultiplePlans
           ? (
@@ -16,9 +25,10 @@ const Plan = ({ listing }) => {
           : (
             <img
               loading='lazy'
-              className='rounded-lg h-full w-full object-cover'
+              className={`rounded-lg h-full w-full object-cover cursor-pointer ${isFullscreen ? 'fullscreen' : ''}`}
               src={listing.plan[0]?.url}
               alt='Plan'
+              onClick={handleImageClick} // Gestisci il clic sull'immagine
             />
             )}
       </div>
