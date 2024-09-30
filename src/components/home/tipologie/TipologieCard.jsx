b1294fbaf167712fbc56da886d3f02bb096b892a
ManuelBellucci <matiasbelluccimanuel@gmail.com>
2024-09-30 10:38:22 +0200

    Refactor TipologieCard component to simplify link handling

--- src/components/home/tipologie/TipologieCard.jsx
+++ src/components/home/tipologie/TipologieCard.jsx
@@ -7,15 +7,7 @@
 */
 const TipologieCard = ({ title, subtitle, href, imgSrc }) => {
   return (
-    <a
-      href={{
-        pathname: href.split('?')[0],
-        search: href.split('?')[1]
-      }}
-      target='_blank'
-      rel='noreferrer'
-      className='flex w-full flex-col justify-center overflow-hidden rounded-lg sm:mx-auto sm:max-w-lg'
-    >
+    <a href={href} target='_blank' rel='noreferrer' className='flex w-full flex-col justify-center overflow-hidden rounded-lg sm:mx-auto sm:max-w-lg'>
       <div className='group relative m-0 flex h-72 w-full xl:h-96'>
         <img loading='lazy' src={imgSrc} alt={title} className='h-full w-full object-cover object-bottom transition-all duration-300 ease-in-out group-hover:scale-110' />
         <div className='absolute bottom-0 text-center text-balance border-t-2 h-24 flex flex-col items-center justify-center border-primary-950 rounded-t-none rounded-lg left-0 z-20 w-full p-2 bg-primary-500'>
